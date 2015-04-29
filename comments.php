8df63f65682d7686b9a13118d9e07a4de6215e9a
Antal Orcsik <antal.orcsik@prezi.com>
2015-04-29 15:22:04 +0200

    change wording thought -> note

--- comments.php
+++ comments.php
@@ -27,7 +27,7 @@ if ( post_password_required() ) {
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'kirumo' ),
+				printf( _nx( 'One note on &ldquo;%2$s&rdquo;', '%1$s notes on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'kirumo' ),
 					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 			?>
 		</h2>
