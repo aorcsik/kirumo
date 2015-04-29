b7d5b797e944e230c14435a3c3e04879a892b864
Antal Orcsik <antal.orcsik@prezi.com>
2015-04-29 15:28:44 +0200

    link title only if not single

--- content.php
+++ content.php
@@ -10,7 +10,13 @@ $format = get_post_format() ? get_post_format() : 'standard' ;
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+	<?php if ($format != "status" && $format != "aside") : ?>
+		<?php if (is_single()) : ?>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<?php else : ?>
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<?php endif; ?>	
+	<?php endif; ?>
 	</header><!-- .entry-header -->
 	<?php if ( has_post_thumbnail() ) {
 		the_post_thumbnail( 'large' );
